c2cfedb1f77f30a0b7365bc71081d5ec9192d422
miko <ptechasa@mail.ccsf.edu>
2019-10-31 02:35:07 -0700

    added scrape data from website

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// Initialize Express app
+//Initialize Express app
 var express = require("express");
 var app = express();
 
-// set the view engine to ejs
+//Set the view engine to ejs
 app.set("view engine", "ejs");
 
 //Require request and cheerio for making the scraping possible
@@ -11,19 +11,19 @@ var cheerio = require("cheerio");
 
 var bodyParser = require("body-parser");
 
-// Sets up the Express app to handle data parsing
+//Set up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-//automatically make a route for every single file in public folder
+//Automatically make a route for every single file in public folder
 app.use(express.static(__dirname + '/public'));
 
-// Database configuration
+//Database configuration
 var mongojs = require("mongojs");
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
-//Hook mongojs configuration to the db variable 
+//Mongojs configuration to the db variable 
 var db = mongojs(databaseUrl, collections);
 db.on("error", function (error) {
     console.log("Database Error:", error);
@@ -33,7 +33,49 @@ app.get("/", function (req, res) {
     res.send("hello")
 });
 
+//Retrieve data from database
+app.get("/all", function (req, res) {
+    db.scrapedData.find({}, function (err, found) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(found);
+        }
+    });
+});
 
+//Scrape data from website
+app.get('/scrape', function (req, res) {
+    request.get("https://technewsinsight.com/category/tech-giants/", function (error, respose, body) {
+        var $ = cheerio.load(body);
+        $(".blog-category").each(function (i, element) {
+            // console.log('hey', i)
+            // console.log('oh', element)
+
+            var title = $(this).children("ul").children("li").children(".blog-post-title-box").text();
+            var link = $(this).children("h2").children("a").attr("href")
+
+            console.log('haloo', title)
+            console.log('haloo2', link)
+
+            if (title && link) {
+                db.scrapedData.save({
+                    title: title,
+                    link: link
+                },
+                    function (err, saved) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.log(saved)
+                        }
+                    })
+            }
+        })
+    })
+
+    res.send("Scrape Sucessful");
+})
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
